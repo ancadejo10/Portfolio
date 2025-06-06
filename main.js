72db9a4e38dd1b35d1f24bf974febd3c3c67f121
Arturo <98238485+ancadejo10@users.noreply.github.com>
2025-06-06 12:48:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var topbar = document.getElementById("Topbar")
+var container = document.getElementById("Container")
+
+var prevBtn
+
+function ActiveBtn(btn, goto){
+    prevBtn.className = ""
+    prevBtn = btn
+
+    btn.className = "Selected"
+
+    if(goto != null){
+        container.src = "Sectors/" + btn.id + ".html#" + goto
+    }else{
+        container.src = "Sectors/" + btn.id + ".html"
+    }
+}
+
+function SetButton(btn){
+    btn.addEventListener("click", function(){
+        ActiveBtn(btn)
+    })
+}
+
+for(let i = 0; i < topbar.children.length; i++){
+    var btn = topbar.children[i]
+
+    if(i == 0){
+        prevBtn = btn
+        ActiveBtn(btn)
+    }
+
+    SetButton(btn)
+}
+
+window.addEventListener('message', (event) => {
+  if (event.data.type === 'goto') {
+    ActiveBtn(topbar.children[event.data.sector], event.data.goto);
+  }
+});
